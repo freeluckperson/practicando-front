2a2875b189a1df6bde4d7c9006df138db4b9c1d8
freeluckperson <gestionecesaria@gmail.com>
2023-11-09 12:53:50 -0400

    Fetch como un pro terminado

--- fetch-axios/src/App.jsx
+++ fetch-axios/src/App.jsx
@@ -1,12 +1,15 @@
 import { useFetch } from "./useFetch";
 
 const App = () => {
-  const { data } = useFetch("https://reqres.in/api/users?per_page=12");
+  const { data, loading, error } = useFetch(
+    "https://reqres.in/api/users?per_page=12"
+  );
 
   return (
     <div>
       <h2 className="bg-primary">Practicando fetch</h2>
-
+      {loading && <li>Loading...</li>}
+      {error && <li>Error... {error}</li>}
       <ul>
         {data?.map((user) => (
           <ol>{`${user.first_name} ${user.last_name}`}</ol>
--- fetch-axios/src/useFetch.js
+++ fetch-axios/src/useFetch.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from "react";
 
 export const useFetch = (url) => {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    setLoading(true);
     fetch(url)
       .then((resp) => resp.json())
       .then((data) => setData(data.data))
-      .catch((error) => console.log(error));
+      .catch((error) => setError(error.message))
+      .finally(() => setLoading(false));
   }, []);
-  return { data };
+  return { data, loading, error };
 };
